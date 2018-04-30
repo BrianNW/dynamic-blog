98cb0172cd3eba29947a0db6a58660ef2ac4b742
Brian W <Briantechdom@gmail.com>
2018-04-29 20:05:42 -0700

    Committing post.php add

---
+++ includes/posts.php
@@ -0,0 +1,21 @@
+<?php
+
+// create an array of blogposts
+
+$post = array(
+          array(
+            "post-title" => "Sample blog post",
+            "post-date" => "January 1, 2018",
+            "post-author" => "Brian",
+            "post-content" => "Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus."
+
+          ),
+          array(
+            "post-title" => "New Feature",
+            "post-date" => "March 1, 2018",
+            "post-author" => "Bronson",
+            "post-content" => "Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros."
+
+          ),
+);
+?>
