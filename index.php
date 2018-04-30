5b162bdd49b8cb4070a2fff1af7406016cb11960
Brian W <Briantechdom@gmail.com>
2018-04-29 19:58:10 -0700

    Fixed includes header and footer issue

--- index.php
+++ index.php
@@ -1,21 +1,5 @@
-<<<<<<< HEAD:index.php
 <?php include('includes/header.php'); ?>
 
-=======
-
-
-<div class="blog-masthead">
-     <div class="container">
-       <nav class="blog-nav">
-         <a class="blog-nav-item active" href="#">Home</a>
-         <a class="blog-nav-item" href="#">New features</a>
-         <a class="blog-nav-item" href="#">Press</a>
-         <a class="blog-nav-item" href="#">New hires</a>
-         <a class="blog-nav-item" href="#">About</a>
-       </nav>
-     </div>
-   </div>
->>>>>>> footer.php:index.php
 
    <div class="container">
 
@@ -131,3 +115,5 @@
      </div><!-- /.row -->
 
    </div><!-- /.container -->
+
+<?php include('includes/footer.php');
